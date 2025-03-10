b7461ac227eafe8c6658183b70a80bf3a3297812
Gustavo Campos <gustavobcampos7@gmail.com>
2025-03-10 16:21:08 -0300

    feat: Add support for sourcing secret environment variables in zsh

--- home.nix
+++ home.nix
@@ -152,6 +152,10 @@
     enable = true;
     enableCompletion = true;
     initExtra = ''
+    if [ -f ~/.secret-env ]; then
+      source ~/.secret-env
+    fi
+
     source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
     source ~/.p10k.zsh
     eval "$(~/.nix-profile/bin/mise activate zsh)"
