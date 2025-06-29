22524926a924da7933cc2739cc6f6ea4ab477840
Gustavo Campos <gustavobcampos7@gmail.com>
2025-06-29 08:31:09 -0300

    sonar

--- home/.config/nvim/lua/user/plugins/sonar.lua
+++ home/.config/nvim/lua/user/plugins/sonar.lua
@@ -1,1 +1,27 @@
-return { url = "https://gitlab.com/schrieveslaach/sonarlint.nvim" }
+return {
+  url = "https://gitlab.com/schrieveslaach/sonarlint.nvim",
+  ft = { "javascript", "typescript", "typescriptreact" },
+  dependencies = {
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    require("sonarlint").setup {
+      server = {
+        cmd = {
+          "sonarlint-ls",
+          -- Ensure that sonarlint-language-server uses stdio channel
+          "-stdio",
+          "-analyzers",
+          -- paths to the analyzers you need, using those for python and java in this example
+          vim.fn.expand "$SONAR_LINT_HOME/share/plugins/sonarjs.jar",
+        },
+      },
+      filetypes = {
+        "javascript",
+        "typescript",
+        "typescriptreact",
+        "css",
+      },
+    }
+  end,
+}
